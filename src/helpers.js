d90f18f35a7e13de206970b0d4b179a74ad8712b
GitStearis <egor.puisha@gmail.com>
2018-12-08 00:04:54 +0300

    Concat participants using empty line

--- src/helpers.js
+++ src/helpers.js
@@ -1,4 +1,4 @@
 module.exports.buildList = (placesLeft, usernames) => {
-  const lines = usernames.map((username, index) => `${index + 1}. @${username}\n`).join();
+  const lines = usernames.map((username, index) => `${index + 1}. @${username}\n`).join('');
   return `🎅 Осталось мест: ${placesLeft}\n\n🗒 Список Тайных Сант:\n\n${lines}`;
 }
